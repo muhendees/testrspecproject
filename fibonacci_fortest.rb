d7f24ea6f7936b9a71996ce3ddf9aa7befac23a0
Muhendees <muhendees@gmail.com>
2015-03-19 10:15:29 +0400

    added new fibo script

---
+++ fibonacci_fortest.rb
@@ -0,0 +1,16 @@
+def fib(n)
+  return n if (0..1).include? n
+  fib(n-1) + fib(n-2) if n > 1
+end
+
+a = 0
+thrdz = []
+10.times do
+  thrdz << Thread.new do
+    a = fib(44)
+  end
+end
+
+thrdz.each(&:join)
+
+puts a
